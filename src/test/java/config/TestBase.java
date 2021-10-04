e8145774eb6b51be4912c87e2e41dc093c6d52e2
Kratakey <Kratakey@gmail.com>
2021-10-04 04:20:58 +0300

    fixes

--- src/test/java/config/TestBase.java
+++ src/test/java/config/TestBase.java
@@ -4,12 +4,18 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static java.lang.String.format;
 
 
 public class TestBase {
+    public static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+
     @BeforeAll
     public static void browserInitialConfiguration() {
             SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
@@ -20,7 +26,13 @@ public class TestBase {
 
             Configuration.browserCapabilities = capabilities;
             Configuration.startMaximized = true;
-            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+//            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+            Configuration.remote = format("https://%s:%s@%s", credentials.login(), credentials.password(),credentials.url());
+    }
+
+    @Test
+    void dsa() {
+        System.out.println(credentials.login()+credentials.password()+credentials.url());
     }
 
     @AfterEach
